a9a468a74b35a50f11be2695b5a647c0b0299654
GonzaloHn <101721224+GonzaloHn@users.noreply.github.com>
2022-03-30 19:00:08 -0300

    Exercise 3

---
+++ Assets/Scripts/Ejercicio03.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ejercicio03 : MonoBehaviour
+{
+    string name;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Hola" + name);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
